f800bd192ae6ca5d6cf83fc089a9550d6b9d897d
arrilight <g.dzesov@gmail.com>
2018-04-03 17:52:01 +0300

    Exact solution added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,51 +1,46 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart.Data;
 import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.TextField;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import java.util.ArrayList;
 
-public class Controller implements Initializable {
+public class Controller {
     @FXML
-    private LineChart<Number, Number> MyChart;
+    private LineChart<Number, Number> Graph;
+    @FXML
+    private TextField initial_x;
+    @FXML
+    private TextField initial_y;
+    @FXML
+    private TextField initial_X;
+    @FXML
+    private TextField initial_N;
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        double x0 = -Math.PI, X = Math.PI;
-        int N = 50;
-        double x[] = new double[N];
-        double y_sin[] = new double[N];
-        double y_cos[] = new double[N];
 
-        double h = (X - x0) / N;
-
-        x[0]=x0;
-        for (int i = 1; i < N; i++) {
-            x[i] = x[i-1] + h;
+    @FXML
+    private void handleApplyAction(ActionEvent event) {
+        Series exactSolution = new Series();
+        exactSolution.setName("Exact");
+        double x0 = Double.parseDouble(initial_x.getText());
+        double X = Double.parseDouble(initial_X.getText());
+        double y0 = Double.parseDouble(initial_y.getText());
+        double N = Double.parseDouble(initial_N.getText());
+        double h = (X - x0)/N;
+        double constant = ExactSolution.calculateConstant(x0, y0);
+        System.out.println(x0 + " " + X + " " + y0 + " " + N + " ");
+        ArrayList<Double> exactValues = ExactSolution.calculateValues(x0, X, h, constant);
+        double x = x0;
+        for (double value:exactValues) {
+            exactSolution.getData().add(new Data(x, value));
+            x += h;
         }
-
-        for (int i = 0; i < N; i++) {
-            y_sin[i] = Math.sin(x[i]);
-            y_cos[i] = Math.cos(x[i]);
-        }
-
-        Series sin_ser = new Series();
-        sin_ser.setName("Sin");
-        Series cos_ser = new Series();
-        cos_ser.setName("Cos");
-
-        for (int i=0; i<x.length; i++)
-        {
-            sin_ser.getData().add(new Data(x[i], y_sin[i]));
-            cos_ser.getData().add(new Data(x[i], y_cos[i]));
-        }
-
-        MyChart.getData().addAll(sin_ser, cos_ser);
-
+        Graph.getData().clear();
+        Graph.getData().addAll(exactSolution);
     }
 
 }
--- src/sample/ExactSolution.java
+++ src/sample/ExactSolution.java
@@ -1,4 +1,25 @@
 package sample;
 
+import java.util.ArrayList;
+
 public class ExactSolution {
+
+    public static double calculateConstant(double x0, double y0) {
+        return (Math.log((y0 - 2*x0)/(Math.pow(x0, 3) * (y0 + x0))))/3;
+
+    }
+
+    public static ArrayList<Double> calculateValues(double x0, double X, double h, double c) {
+        ArrayList<Double> result = new ArrayList<>();
+        double x = x0;
+        while (x <= X) {
+            result.add(calculateValue(c, x));
+            x += h;
+        }
+        return result;
+    }
+
+    private static double calculateValue(double c, double x) {
+        return -(x * Math.exp(3 * c) * Math.pow(x, 3) + 2)/(Math.exp(3 * c) * Math.pow(x, 3) - 1);
+    }
 }
